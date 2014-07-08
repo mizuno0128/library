86b92c9b4c19d1b8f9719f716e196ca88e8173e3
mizunoyu <mizunoyuuka0128@yahoo.co.jp>
2014-07-08 14:00:34 +0900

    返却を延滞した場合の表示

--- app/helpers/book_records_helper.rb
+++ app/helpers/book_records_helper.rb
@@ -7,4 +7,11 @@ module BookRecordsHelper
   def return_book?(date, flag)
     flag == false && date.nil?
   end
+
+  def expire?(date, r_date)
+    #binding.pry
+    if date <= Date.today && r_date == nil
+      return true
+    end 
+  end
 end
